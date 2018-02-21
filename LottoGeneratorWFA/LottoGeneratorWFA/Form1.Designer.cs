285c7a19b161ffdb5339e9d7cfa2a15f876af926
Johanna Merikanto <johanna.merikanto2@pointcollege.fi>
2018-02-21 10:46:52 +0200

    Lisätty koodit ja taustakuva.

--- LottoGeneratorWFA/LottoGeneratorWFA/Form1.Designer.cs
+++ LottoGeneratorWFA/LottoGeneratorWFA/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.num1 = new System.Windows.Forms.TextBox();
             this.num7 = new System.Windows.Forms.TextBox();
             this.num6 = new System.Windows.Forms.TextBox();
@@ -36,89 +37,116 @@
             this.num3 = new System.Windows.Forms.TextBox();
             this.num2 = new System.Windows.Forms.TextBox();
             this.lottobtn = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lottoLbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // num1
             // 
-            this.num1.Location = new System.Drawing.Point(22, 64);
+            this.num1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.num1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num1.Location = new System.Drawing.Point(28, 136);
             this.num1.Multiline = true;
             this.num1.Name = "num1";
             this.num1.Size = new System.Drawing.Size(84, 50);
             this.num1.TabIndex = 0;
+            this.num1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // num7
             // 
-            this.num7.Location = new System.Drawing.Point(563, 64);
+            this.num7.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.num7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num7.Location = new System.Drawing.Point(569, 136);
             this.num7.Multiline = true;
             this.num7.Name = "num7";
             this.num7.Size = new System.Drawing.Size(84, 50);
             this.num7.TabIndex = 1;
+            this.num7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // num6
             // 
-            this.num6.Location = new System.Drawing.Point(472, 64);
+            this.num6.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.num6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num6.Location = new System.Drawing.Point(478, 136);
             this.num6.Multiline = true;
             this.num6.Name = "num6";
             this.num6.Size = new System.Drawing.Size(84, 50);
             this.num6.TabIndex = 2;
+            this.num6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // num5
             // 
-            this.num5.Location = new System.Drawing.Point(382, 64);
+            this.num5.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.num5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num5.Location = new System.Drawing.Point(388, 136);
             this.num5.Multiline = true;
             this.num5.Name = "num5";
             this.num5.Size = new System.Drawing.Size(84, 50);
             this.num5.TabIndex = 3;
+            this.num5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // num4
             // 
-            this.num4.Location = new System.Drawing.Point(292, 64);
+            this.num4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.num4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num4.Location = new System.Drawing.Point(298, 136);
             this.num4.Multiline = true;
             this.num4.Name = "num4";
             this.num4.Size = new System.Drawing.Size(84, 50);
             this.num4.TabIndex = 4;
+            this.num4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // num3
             // 
-            this.num3.Location = new System.Drawing.Point(202, 64);
+            this.num3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.num3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num3.Location = new System.Drawing.Point(208, 136);
             this.num3.Multiline = true;
             this.num3.Name = "num3";
             this.num3.Size = new System.Drawing.Size(84, 50);
             this.num3.TabIndex = 5;
+            this.num3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // num2
             // 
-            this.num2.Location = new System.Drawing.Point(112, 64);
+            this.num2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.num2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num2.Location = new System.Drawing.Point(118, 136);
             this.num2.Multiline = true;
             this.num2.Name = "num2";
             this.num2.Size = new System.Drawing.Size(84, 50);
             this.num2.TabIndex = 6;
+            this.num2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // lottobtn
             // 
-            this.lottobtn.Location = new System.Drawing.Point(229, 156);
+            this.lottobtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lottobtn.Location = new System.Drawing.Point(235, 228);
             this.lottobtn.Name = "lottobtn";
             this.lottobtn.Size = new System.Drawing.Size(179, 68);
             this.lottobtn.TabIndex = 7;
             this.lottobtn.Text = "LOAD LOTTO NUMBERS";
             this.lottobtn.UseVisualStyleBackColor = true;
+            this.lottobtn.Click += new System.EventHandler(this.lottobtn_Click);
             // 
-            // label1
+            // lottoLbl
             // 
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(227, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(181, 35);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "Lotto Numbers";
+            this.lottoLbl.BackColor = System.Drawing.SystemColors.Window;
+            this.lottoLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lottoLbl.Location = new System.Drawing.Point(233, 46);
+            this.lottoLbl.Name = "lottoLbl";
+            this.lottoLbl.Size = new System.Drawing.Size(181, 35);
+            this.lottoLbl.TabIndex = 8;
+            this.lottoLbl.Text = "Lotto Numbers";
+            this.lottoLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(659, 406);
-            this.Controls.Add(this.label1);
+            this.AutoSize = true;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(674, 601);
+            this.Controls.Add(this.lottoLbl);
             this.Controls.Add(this.lottobtn);
             this.Controls.Add(this.num2);
             this.Controls.Add(this.num3);
@@ -127,8 +155,10 @@
             this.Controls.Add(this.num6);
             this.Controls.Add(this.num7);
             this.Controls.Add(this.num1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lotto Number Generator ";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -144,7 +174,7 @@
         private System.Windows.Forms.TextBox num3;
         private System.Windows.Forms.TextBox num2;
         private System.Windows.Forms.Button lottobtn;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lottoLbl;
     }
 }
 
--- LottoGeneratorWFA/LottoGeneratorWFA/Form1.cs
+++ LottoGeneratorWFA/LottoGeneratorWFA/Form1.cs
@@ -20,5 +20,24 @@ namespace LottoGeneratorWFA
         {
             InitializeComponent();
         }
+
+        private void lottobtn_Click(object sender, EventArgs e)
+        {
+            int a = rand.Next(1, 40);
+            int b = rand.Next(1, 40);
+            int c = rand.Next(1, 40);
+            int d = rand.Next(1, 40);
+            int f = rand.Next(1, 40);
+            int g = rand.Next(1, 40);
+            int h = rand.Next(1, 40);
+
+            num1.Text = a.ToString();
+            num2.Text = b.ToString();
+            num3.Text = c.ToString();
+            num4.Text = d.ToString();
+            num5.Text = f.ToString();
+            num6.Text = g.ToString();
+            num7.Text = h.ToString(); 
+        }
     }
 }
