b7b3a035e28b063750d68eaff1fb9d59b9429abf
Johanna Merikanto <johanna.merikanto2@pointcollege.fi>
2018-02-21 10:00:02 +0200

    Luotu uusi lottonumero generaattori, joka arpoo satunnaisesti 7 numeroa välillä 1-40.

---
+++ LottoGeneratorWFA/LottoGeneratorWFA/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace LottoGeneratorWFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num1 = new System.Windows.Forms.TextBox();
+            this.num7 = new System.Windows.Forms.TextBox();
+            this.num6 = new System.Windows.Forms.TextBox();
+            this.num5 = new System.Windows.Forms.TextBox();
+            this.num4 = new System.Windows.Forms.TextBox();
+            this.num3 = new System.Windows.Forms.TextBox();
+            this.num2 = new System.Windows.Forms.TextBox();
+            this.lottobtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(22, 64);
+            this.num1.Multiline = true;
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(84, 50);
+            this.num1.TabIndex = 0;
+            // 
+            // num7
+            // 
+            this.num7.Location = new System.Drawing.Point(563, 64);
+            this.num7.Multiline = true;
+            this.num7.Name = "num7";
+            this.num7.Size = new System.Drawing.Size(84, 50);
+            this.num7.TabIndex = 1;
+            // 
+            // num6
+            // 
+            this.num6.Location = new System.Drawing.Point(472, 64);
+            this.num6.Multiline = true;
+            this.num6.Name = "num6";
+            this.num6.Size = new System.Drawing.Size(84, 50);
+            this.num6.TabIndex = 2;
+            // 
+            // num5
+            // 
+            this.num5.Location = new System.Drawing.Point(382, 64);
+            this.num5.Multiline = true;
+            this.num5.Name = "num5";
+            this.num5.Size = new System.Drawing.Size(84, 50);
+            this.num5.TabIndex = 3;
+            // 
+            // num4
+            // 
+            this.num4.Location = new System.Drawing.Point(292, 64);
+            this.num4.Multiline = true;
+            this.num4.Name = "num4";
+            this.num4.Size = new System.Drawing.Size(84, 50);
+            this.num4.TabIndex = 4;
+            // 
+            // num3
+            // 
+            this.num3.Location = new System.Drawing.Point(202, 64);
+            this.num3.Multiline = true;
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(84, 50);
+            this.num3.TabIndex = 5;
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(112, 64);
+            this.num2.Multiline = true;
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(84, 50);
+            this.num2.TabIndex = 6;
+            // 
+            // lottobtn
+            // 
+            this.lottobtn.Location = new System.Drawing.Point(229, 156);
+            this.lottobtn.Name = "lottobtn";
+            this.lottobtn.Size = new System.Drawing.Size(179, 68);
+            this.lottobtn.TabIndex = 7;
+            this.lottobtn.Text = "LOAD LOTTO NUMBERS";
+            this.lottobtn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(227, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 35);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Lotto Numbers";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 406);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lottobtn);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num3);
+            this.Controls.Add(this.num4);
+            this.Controls.Add(this.num5);
+            this.Controls.Add(this.num6);
+            this.Controls.Add(this.num7);
+            this.Controls.Add(this.num1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox num1;
+        private System.Windows.Forms.TextBox num7;
+        private System.Windows.Forms.TextBox num6;
+        private System.Windows.Forms.TextBox num5;
+        private System.Windows.Forms.TextBox num4;
+        private System.Windows.Forms.TextBox num3;
+        private System.Windows.Forms.TextBox num2;
+        private System.Windows.Forms.Button lottobtn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
